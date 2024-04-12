92eaf00d133b9816f862c4a0f421480021b77d93
Wilmer Perdomo <wilmerpb30@gmail.com>
2024-04-12 18:20:02 -0500

    update

--- tutornet/tutorias/api/services/estudiante.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from tutorias.models import Estudiante
-from tutorias.api.serializers import EstudianteSerializer
-
-class EstudianteViewSet(viewsets.ModelViewSet):
-    """Conjunto de vistas para el modelo Estudiante."""
-    queryset = Estudiante.objects.all()
-    serializer_class = EstudianteSerializer
--- tutornet/tutorias/api/services/tutor.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from tutorias.models import Tutor
-from tutorias.api.serializers import TutorSerializer
-
-class TutorViewSet(viewsets.ModelViewSet):
-    """Conjunto de vistas para el modelo Tutor."""
-    queryset = Tutor.objects.all()
-    serializer_class = TutorSerializer
